371e51d7c5b50ec8fe3845e96ec99b9f6524d193
Artem_Guna <Artemdvurecenskih83@gmail.com>
2025-05-16 14:13:19 +0300

    Currency transfer logic

---
+++ app/src/main/java/com/example/artemka/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package com.example.artemka;
+
+import com.example.artemka.Currency;
+public class CurrencyConverter {
+    public static double convert(double amount, Currency from, Currency to) {
+        // Конвертируем через рубль как базовую валюту
+        double amountInRub = amount * from.getRate();
+        return amountInRub / to.getRate();
+
+    }
+}
